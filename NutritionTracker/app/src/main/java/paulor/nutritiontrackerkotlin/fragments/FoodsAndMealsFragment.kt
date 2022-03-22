75d9fbc5be05101e716d3ba8bfe3993794ecc220
p4ulor <A44873@alunos.isel.pt>
2022-03-22 13:30:46 +0000

    Tried adding gif on alert dialog on adding edible button

--- NutritionTracker/app/src/main/java/paulor/nutritiontrackerkotlin/fragments/FoodsAndMealsFragment.kt
+++ NutritionTracker/app/src/main/java/paulor/nutritiontrackerkotlin/fragments/FoodsAndMealsFragment.kt
@@ -1,6 +1,7 @@
 package paulor.nutritiontrackerkotlin.fragments
 
 import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -9,17 +10,20 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import paulor.nutritiontrackerkotlin.MainActivityViewModel
 import paulor.nutritiontrackerkotlin.databinding.FragmentFoodsAndMealsBinding
-import paulor.nutritiontrackerkotlin.log
 import paulor.nutritiontrackerkotlin.views.FoodsAndMealsAdapter
 import paulor.nutritiontrackerkotlin.views.OnItemClickListener
 
 import android.view.*
-import android.widget.EditText
-import paulor.nutritiontrackerkotlin.R
-import paulor.nutritiontrackerkotlin.mapper
+import android.widget.*
+import paulor.nutritiontrackerkotlin.*
+import paulor.nutritiontrackerkotlin.model.Compound
 import paulor.nutritiontrackerkotlin.model.Food
+import paulor.nutritiontrackerkotlin.model.Nutrient
+import pl.droidsonroids.gif.GifImageView
+import pl.droidsonroids.gif.GifDrawable
+import java.io.File
+
 
 private const val TAG = "FoodsAndMealsFragment"
 
@@ -75,6 +79,17 @@ class FoodsAndMealsFragment : Fragment(), OnItemClickListener {
                 }
                 .create()
                 .show()
+        }
+
+        binding.addNewEdibleButton.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCanceledOnTouchOutside(true) //Sets whether this dialog is canceled when touched outside the window's bounds. If setting to true, the dialog is set to be cancelable if not already set
+            dialog.setContentView(R.layout.dialog_add_edible)
+            val gif = dialog.findViewById<GifImageView>(R.id.gifImageView)
+            //val gifFromAssets = GifDrawable(resources, R.drawable.food)
+            //gif.background = gifFromAssets
+            dialog.show()
         }
 
         viewModel.foods?.observe(viewLifecycleOwner) {
